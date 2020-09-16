446d9c5e68a4e5d97ffd54c97f611e6992291c2a
JonatanMorales <68953969+JonatanMorales@users.noreply.github.com>
2020-09-15 23:54:24 -0300

    Add files via upload
    formulario con ABM

---
+++ Proyecto-Bugs-Extendido-master_v1/Proyecto Bugs Extendido/Datos/ProductoDao.cs
@@ -0,0 +1,41 @@
+﻿using BugTracker;
+using Proyecto_Bugs_Extendido.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Bugs_Extendido.Datos
+{
+    class ProductoDao
+    {
+        public IList<Producto> GetAll()
+        {
+            List<Producto> listadoBugs = new List<Producto>();
+
+            var strSql = "SELECT * From Productos WHERE borrado=0";
+
+            var resultadoConsulta = DBHelper.GetDBHelper().ConsultaSQL(strSql);
+
+            foreach (DataRow row in resultadoConsulta.Rows)
+            {
+                listadoBugs.Add(ObjectMapping(row));
+            }
+
+            return listadoBugs;
+        }
+
+        private Producto ObjectMapping(DataRow row)
+        {
+            Producto oProducto = new Producto
+            {
+                Id_producto = Convert.ToInt32(row["id_producto"].ToString()),
+                Nombre = row["nombre"].ToString()
+            };
+
+            return oProducto;
+        }
+    }
+}
---
+++ Proyecto-Bugs-Extendido-master_v1/Proyecto Bugs Extendido/Negocio/ProductoServicio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto_Bugs_Extendido.Datos;
+using Proyecto_Bugs_Extendido.Entidades;
+
+namespace Proyecto_Bugs_Extendido.Negocio
+{
+    class ProductoServicio
+    {
+        private ProductoDao oProductoDao;
+        public ProductoServicio()
+        {
+            this.oProductoDao = new ProductoDao();
+        }
+        public IList<Producto> ObtenerTodos()
+        {
+            return oProductoDao.GetAll();
+        }
+    }
+}
